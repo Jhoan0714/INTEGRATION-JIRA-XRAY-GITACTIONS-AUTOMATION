67d8865addac61572f4deba28f0455e8a7fb846c
Jhoan0714 <jflopez52@ucatolica.edu.co>
2020-08-11 13:16:11 -0500

    Execute docker test and ultimate test

--- robot_tests/configure_test.sh
+++ robot_tests/configure_test.sh
@@ -5,7 +5,7 @@ LOG_PATH=log/cameo
 LOGO_CDFI=https://cdn-source-cameo.lendingfront.com/static/images/resources/qa/login-logo.png
 NAME_REPORT=cameo.html
 
-for f in $(find /robot_tests -type f -mmin -1); do
+for f in $(find robot_tests -type f -mmin -1); do
         mv $f ${f%.*}.robot
     docker run --name "$CONT_NAME" --shm-size=256m -i \
         -v $(pwd):/home \
