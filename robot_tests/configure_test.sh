e11aaedc32fe90a544d41a07293b975443e26d37
Jhoan0714 <jflopez52@ucatolica.edu.co>
2020-08-11 13:32:04 -0500

    Execute docker test and ultimate test

--- robot_tests/configure_test.sh
+++ robot_tests/configure_test.sh
@@ -5,15 +5,16 @@ LOG_PATH=log/cameo
 LOGO_CDFI=https://cdn-source-cameo.lendingfront.com/static/images/resources/qa/login-logo.png
 NAME_REPORT=cameo.html
 
-for f in $(find $1 -type f -mmin -1); do
-        mv $f ${f%.*}.robot
+for f in $(find $1 -type f -mmin -1 \! -name "*.sh"); do
+    mv $f ${f%.*}.robot
+    docker rm -f "$CONT_NAME"
     docker run --name "$CONT_NAME" --shm-size=256m -i \
         -v $(pwd):/home \
         -e BUILD_NUMBER=$BUILD_NUMBER \
         -e RESOURCES_PATH=/home/resources \
         lendingfront/robotframework-selenium:2.5.1 \
-            $LOG_PATH \
-            $1/${f%.*}.robot \
-            $LOGO_CDFI \
-            $NAME_REPORT
+        $LOG_PATH \
+        ${f%.*}.robot \
+        $LOGO_CDFI \
+        $NAME_REPORT
 done
