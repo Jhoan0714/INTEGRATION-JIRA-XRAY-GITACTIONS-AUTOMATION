c80959deeda9623a1451e52c09ff33e077514e13
Jhoan0714 <jflopez52@ucatolica.edu.co>
2020-08-11 13:08:31 -0500

    Exucte docker test and ultimate test

---
+++ robot_tests/configure_test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -
+CONT_NAME=test-githubactions-individual
+BUILD_NUMBER=99
+LOG_PATH=log/cameo
+LOGO_CDFI=https://cdn-source-cameo.lendingfront.com/static/images/resources/qa/login-logo.png
+NAME_REPORT=cameo.html
+
+for f in $(find / -type f -mmin -1); do
+        mv $f ${f%.*}.robot
+    docker run --name "$CONT_NAME" --shm-size=256m -i \
+        -v $(pwd):/home \
+        -e BUILD_NUMBER=$BUILD_NUMBER \
+        -e RESOURCES_PATH=/home/resources \
+        lendingfront/robotframework-selenium:$(cat container-version) \
+            $LOG_PATH \
+            ${f%.*}.robot \
+            $LOGO_CDFI \
+            $NAME_REPORT
+done
