372b33bdf19e4935d2b8f3492e8bab2f03be55b0
Jhoan0714 <jflopez52@ucatolica.edu.co>
2020-08-11 13:52:35 -0500

    Change sh

--- robot_tests/configure_test.sh
+++ robot_tests/configure_test.sh
@@ -5,19 +5,16 @@ LOG_PATH=log/cameo
 LOGO_CDFI=https://cdn-source-cameo.lendingfront.com/static/images/resources/qa/login-logo.png
 NAME_REPORT=cameo.html
 
-for f in $(find $1 -type f -mmin -1); do
+for f in $(find $1 -type f -mmin -1 \! -name "*.sh"); do
     mv $f ${f%.*}.robot
-    ls -l
-    #docker rm -f "$CONT_NAME"
-    #docker run --name "$CONT_NAME" --shm-size=256m -i \
-    #    -v $(pwd):/home \
-    #    -e BUILD_NUMBER=$BUILD_NUMBER \
-    #    -e RESOURCES_PATH=/home/resources \
-    #    lendingfront/robotframework-selenium:2.5.1 \
-    #    $LOG_PATH \
-    #    ${f%.*}.robot \
-    #    $LOGO_CDFI \
-    #    $NAME_REPORT
+    docker rm -f "$CONT_NAME"
+    docker run --name "$CONT_NAME" --shm-size=256m -i \
+        -v $(pwd):/home \
+        -e BUILD_NUMBER=$BUILD_NUMBER \
+        -e RESOURCES_PATH=/home/resources \
+        lendingfront/robotframework-selenium:2.5.1 \
+        $LOG_PATH \
+        ${f%.*}.robot \
+        $LOGO_CDFI \
+        $NAME_REPORT
 done
-
-sh configure_test.sh ${{ github.workspace }}/robot_tests
