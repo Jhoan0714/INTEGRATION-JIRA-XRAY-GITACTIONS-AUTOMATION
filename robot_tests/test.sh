55fa84efc5d92c4edb22e45bfbcb0c51c3500454
Jhoan0714 <jflopez52@ucatolica.edu.co>
2020-08-13 12:54:03 -0500

    Convert json

--- robot_tests/test.sh
+++ robot_tests/test.sh
@@ -1,4 +1,5 @@
 COMMIT="https://api.github.com/repos/Jhoan0714/INTEGRATION-JIRA-XRAY-GITACTIONS-AUTOMATION/commits{/sha}"
-SHA="/estemisha"
+SHA="/eda378ab8d4132b4fc6a34d65b9f75dfce18f34f"
 COMMIT_URL=$(echo "${COMMIT/{\/sha\}/$SHA}")
-echo $COMMIT_URL
+COMMIT_CONTEXT=$(curl $COMMIT_URL)
+echo $COMMIT_JSON
