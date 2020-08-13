634ce5757172dbdaa6e7b9759a0ce12d30fc044d
Jhoan0714 <jflopez52@ucatolica.edu.co>
2020-08-13 16:44:51 -0500

    Added yml execution test individual

--- robot_tests/test.sh
+++ robot_tests/test.sh
@@ -1,5 +1,11 @@
-COMMIT="https://api.github.com/repos/Jhoan0714/INTEGRATION-JIRA-XRAY-GITACTIONS-AUTOMATION/commits{/sha}"
-SHA="/eda378ab8d4132b4fc6a34d65b9f75dfce18f34f"
-COMMIT_URL=$(echo "${COMMIT/{\/sha\}/$SHA}")
-COMMIT_CONTEXT=$(curl $COMMIT_URL)
-echo $COMMIT_JSON
+FICHERO="home/projects/abcd.story"
+basename $FICHERO
+dirname $FICHERO
+echo "${FICHERO%.*}"
+
+
+#COMMIT="https://api.github.com/repos/Jhoan0714/INTEGRATION-JIRA-XRAY-GITACTIONS-AUTOMATION/commits{/sha}"
+#SHA="/eda378ab8d4132b4fc6a34d65b9f75dfce18f34f"
+#COMMIT_URL=$(echo "${COMMIT/{\/sha\}/$SHA}")
+#COMMIT_CONTEXT=$(curl $COMMIT_URL)
+#echo $COMMIT_JSON
